41fcc559263b14f61c903a20f3e03bb786cc2100
Rafael Telles <rafael.telles1@hotmail.com>
2025-10-30 19:59:46 -0400

    Actualizaciones del index
    Actualizaciones basicas del index para llenarle contenido falso de ipsum

--- header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="es">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/Proyectoquinto/Images/logo.ico"/>
-    <link rel="stylesheet" href="/Proyectoquinto/CSS/semantic.min.css">
-
