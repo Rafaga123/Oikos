b7dd5a5027d99d797d2ea2090a99a29de8cd5aeb
sof <sofh080612@gmail.com>
2025-12-08 02:21:25 -0400

    Cambios
    Reestructuración de Index. Mejora del Foro

--- Scripts/home_page.js
+++ Scripts/home_page.js
@@ -1,28 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     initSidebar();
     initAnnouncementCarousel();
+
+    //Inicializar dropdowns
+    $('.ui.dropdown').dropdown();
 });
 
 
 //SIDEBAR
 function initSidebar() {
-    //Seleccion los elementos por su clase
-    const sidebar = document.querySelector('.ui.left.vertical.inverted.sidebar.labeled.icon.menu');
-    const pusher = document.querySelector('.pusher');
-    //Seleccionamos todos los botones de alternancia (el del menú superior y el del sidebar)
-    const toggleButtons = document.querySelectorAll('.sidebar-toggle-btn');
-    
-    //Funcion para manejar los eventos (click)
-    function toggleSidebar() {
-        //se añade la clase si no existe, o se elimina si existe
-        sidebar.classList.toggle('visible');  //El sidebar se muestra u oculta
-        pusher.classList.toggle('pushed'); //Empuja el contenido principal o vuelve a su posición original
-        document.body.classList.toggle('dimmed'); //El body se atenúa cuando el sidebar está visible
-    }
-    
-    //Asigna la función a todos los botones que tienen la clase
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', toggleSidebar); //Ejecuta la función al hacer click
+
+    //Botones de control
+    const allToggleSelectors = '#sidebar-toggle, .sidebar-toggle-btn';
+
+    //Inicializamos y controlamos el sidebar usando la API de Semantic UI
+    $('.ui.sidebar').sidebar({
+        context: $('.pusher'),
+        transition: 'overlay'
+    });
+
+    $(allToggleSelectors).click(function() {
+        //La función 'toggle' lo abrirá si está cerrado, y lo cerrará si está abierto.
+        $('.ui.sidebar').sidebar('toggle');
     });
 }
 
