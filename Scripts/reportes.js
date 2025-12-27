f524c273c95103ffad51f82dc88f3500191d79d1
sof <sofh080612@gmail.com>
2025-12-26 21:29:32 -0400

    Pagina Reporte

---
+++ Scripts/reportes.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    initSidebar();
+});
+
+//SIDEBAR
+function initSidebar() {
+
+    //Botones de control
+    const allToggleSelectors = '#sidebar-toggle, .sidebar-toggle-btn';
+
+    //Inicializamos y controlamos el sidebar usando la API de Semantic UI
+    $('.ui.sidebar').sidebar({
+        context: $('.pusher'),
+        transition: 'overlay'
+    });
+
+    $(allToggleSelectors).click(function() {
+        //La función 'toggle' lo abrirá si está cerrado, y lo cerrará si está abierto.
+        $('.ui.sidebar').sidebar('toggle');
+    });
+}
